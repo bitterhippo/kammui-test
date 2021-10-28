59cd130580af8716b76870b20b870f8f58f2eb12
Noah <bitterhippo@gmail.com>
2021-10-29 08:31:27 +0900

    updated landing - lazyLoad

--- src/Landing.js
+++ src/Landing.js
@@ -1,11 +1,10 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import {
   BrowserRouter as Router,
   Switch,
   Route,
   Link
 } from "react-router-dom";
-import Background from "./Assets/kammui_background.png"
 
 //Screens
 import Home from './Screens/Home';
@@ -15,12 +14,15 @@ import About from './Screens/About';
 //Components
 import NavBar from './Components/NavBar';
 import BackgroundImage from './Assets/BackgroundImage';
+const LazyBackground = React.lazy(() => import("./Assets/BackgroundImage"));
 
 const Landing = () => {
   return (
     <div style={styles.mainWrapper}>
       <Router>
-      <BackgroundImage />
+        <Suspense fallback={<div>Background loading...</div>}>
+          <LazyBackground />
+        </Suspense>
         <NavBar />
         {/* pages need to be added here */}
         <Switch>
@@ -35,6 +37,6 @@ const Landing = () => {
 
 const styles = {
   mainWrapper: {}
-};  
+};
 
 export default Landing;
