feea4ec07b9d1e4aa1f238444c06406454a6285a
Noah <bitterhippo@gmail.com>
2021-10-29 00:21:34 +0900

    created a text input

--- src/Screens/About.js
+++ src/Screens/About.js
@@ -3,15 +3,31 @@ import React from 'react';
 const About = () => {
   return (
     <div style={styles.contentWrapper}>
-      about data
+      <div style={styles.bigText}>About Kammui</div>
+      <div style={styles.smallText}>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. Ipsum.</div>
     </div>
   )
 };
 
 const styles = {
   contentWrapper: {
-    backgroundColor: 'red'
+    marginTop: '15%',
+    marginLeft: 'auto',
+    marginRight: 'auto',
+    width: '60%',
+    textAlign: 'center'
+  },
+  bigText: {
+    fontSize: '52px',
+    fontWeight: 'bold',
+    color: 'white'
+  },
+  smallText: {
+    marginTop: '20px',
+    fontSize: '28px',
+    color: '#DCDCDC',
+    marginBottom: '20px'
   }
-};
+}
 
 export default About;
--- src/Screens/Registration.js
+++ src/Screens/Registration.js
@@ -3,14 +3,21 @@ import React from 'react';
 const Registration = () => {
   return (
     <div style={styles.formWrapper}> 
-      SOME FORm
+      <label> LOL </label>
+      <input type="text" placeholder="lol"></input>
     </div>
   )
 };
 
 const styles = {
   formWrapper: {
-
+    marginTop: '15%',
+    marginLeft: 'auto',
+    marginRight: 'auto',
+    width: '60%',
+    backgroundColor: 'white',
+    borderTop: '4px solid #d387ab',
+    borderBottom: '4px solid #d387ab'
   }
 };
 
